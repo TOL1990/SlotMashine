bd6b1f09bbd6318b35c564b2f2cfab160e96e59a
TOL1990 <olefir.taras@mail.ru>
2016-06-01 18:56:44 +0300

    add lines

--- src/main/java/com/spalah/cources/model/Line.java
+++ src/main/java/com/spalah/cources/model/Line.java
@@ -1,7 +1,10 @@
 package com.spalah.cources.model;
 
+import java.util.Map;
+
 /**
  * Класс описывает линию как перечень индексов двумерного массива.
  */
 public class Line {
+    Map<Integer, Integer> lines; // строки и колонки
 }
