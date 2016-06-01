d91b14ab7f7242a2b5de77a33eec984574c9c329
TOL1990 <olefir.taras@mail.ru>
2016-06-01 20:57:25 +0300

    sql db.

--- src/main/java/com/spalah/cources/model/Mashine.java
+++ src/main/java/com/spalah/cources/model/Mashine.java
@@ -1,14 +1,14 @@
 package com.spalah.cources.model;
 
 
-import com.spalah.cources.model.entity.User;
+import com.spalah.cources.model.entity.Player;
 
 import java.util.List;
 
 public class Mashine {
     List<Line> lines; //перечень линий доступных для игры
     Board drums; // наше игровое поле. Барабаны
-    User player;
+    Player player;
     List<Bet> bets; // Описывает ставки сделанные игроком
 
     public void spin(){}
